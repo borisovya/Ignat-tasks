a26b7a7d33e6cb870d16fbde7451cd6df861032b
Vladimir Borisov <1mt-2@mail.ru>
2022-08-03 12:31:49 +0300

    added hw3

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
+
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType>
+    addUserCallback: (name: string) => void
 }
 
 // более простой и понятный для новичков
@@ -12,17 +14,27 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('')
+    const [error, setError] = useState<string>('')
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+        if (e.currentTarget.value !== '') {
+            setError('')
+        }
     }
+
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        if (name.length) {
+            addUserCallback(name)
+            alert(`Hello  ${name}!`)
+        } else {
+            setError('Не, ну так не пойдет. Введите имя!!!')
+        }
+        setName('')
     }
 
-    const totalUsers = 0 // need to fix
+    let totalUsers = users.length
 
     return (
         <Greeting
