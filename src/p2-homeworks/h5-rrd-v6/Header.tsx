78c3bd1bc1b5a3bf3283cb0720885526b4dd2ab7
Vladimir Borisov <1mt-2@mail.ru>
2022-08-16 11:15:34 +0300

    added Routs and Header.module.css

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.linkBlockStyle}>
+            <div className={s.linkItem}>
+                <NavLink to='/pre-junior' className={(navData) => navData.isActive ? s.active : "" }> Pre-Junior </NavLink>
+            </div>
+            <div className={s.linkItem}>
+                <NavLink to='/junior' className={(navData) => navData.isActive ? s.active : "" }> Junior </NavLink>
+            </div>
+            <div className={s.linkItem}>
+                <NavLink to='/junior-plus' className={(navData) => navData.isActive ? s.active : "" }> Junior+ </NavLink>
+            </div>
         </div>
     )
 }
